3c5a1fb8b0e6dbe0febaede19a745098f33f97bf
Aspker <aspk3r@gmail.com>
2025-05-18 19:14:25 -0400

    Create levels.js

---
+++ APR-Bot/apr-bot/commands/levels.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { getUserXP } from '../utils/xpManager.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('level')
+  .setDescription('Check your current level and XP');
+
+export async function execute(interaction) {
+  const { level, xp, xpToNext } = getUserXP(interaction.user.id);
+  await interaction.reply(`🌟 You are level ${level} with ${xp}/${xpToNext} XP.`);
+}
