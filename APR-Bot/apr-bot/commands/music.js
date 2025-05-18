abd34598d2be4d5fb78abb4fff1741f393ee4f86
Aspker <aspk3r@gmail.com>
2025-05-18 19:14:47 -0400

    Create music.js

---
+++ APR-Bot/apr-bot/commands/music.js
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from 'discord.js';
+import musicPlayer from '../utils/musicPlayer.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('play')
+  .setDescription('Play a song from YouTube')
+  .addStringOption(option =>
+    option.setName('query')
+      .setDescription('YouTube URL or search query')
+      .setRequired(true)
+  );
+
+export async function execute(interaction) {
+  const query = interaction.options.getString('query');
+  await musicPlayer.play(interaction, query);
+}
