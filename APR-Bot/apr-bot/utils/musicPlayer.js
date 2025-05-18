ca41363b17c6110b051b3747938f42b6d6188824
Aspker <aspk3r@gmail.com>
2025-05-18 19:20:08 -0400

    Create musicPlayer.js

---
+++ APR-Bot/apr-bot/utils/musicPlayer.js
@@ -0,0 +1,7 @@
+// utils/musicPlayer.js
+export default {
+  async play(interaction, query) {
+    // placeholder for music play logic
+    await interaction.reply(`Pretend playing: ${query}`);
+  }
+};
