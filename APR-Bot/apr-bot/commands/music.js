15e65d9c178953a84a21d0fb64cfadb35387080c
Aspker <aspk3r@gmail.com>
2025-05-19 07:42:14 -0400

    Update music.js

--- APR-Bot/apr-bot/commands/music.js
+++ APR-Bot/apr-bot/commands/music.js
@@ -1,17 +1,50 @@
-// commands/music.js
 import { SlashCommandBuilder } from 'discord.js';
 import musicPlayer from '../utils/musicPlayer.js';
 
-export const data = new SlashCommandBuilder()
-  .setName('play')
-  .setDescription('Play a song from YouTube')
-  .addStringOption(option =>
-    option.setName('query')
-      .setDescription('YouTube URL or search query')
-      .setRequired(true)
-  );
+export const data = [
+  new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Play a song from YouTube')
+    .addStringOption(option =>
+      option.setName('query')
+        .setDescription('YouTube title or URL')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('author')
+        .setDescription('(Optional) Author to improve search accuracy')
+        .setRequired(false)),
+
+  new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('Stop music and clear the queue'),
+
+  new SlashCommandBuilder()
+    .setName('skip')
+    .setDescription('Skip to the next song in queue'),
+
+  new SlashCommandBuilder()
+    .setName('queue')
+    .setDescription('View the current music queue'),
+];
 
 export async function execute(interaction) {
-  const query = interaction.options.getString('query');
-  await musicPlayer.play(interaction, query);
+  const commandName = interaction.commandName;
+
+  if (commandName === 'play') {
+    const query = interaction.options.getString('query');
+    const author = interaction.options.getString('author') || '';
+    return musicPlayer.play(interaction, `${query} ${author}`.trim());
+  }
+
+  if (commandName === 'stop') {
+    return musicPlayer.stop(interaction);
+  }
+
+  if (commandName === 'skip') {
+    return musicPlayer.skip(interaction);
+  }
+
+  if (commandName === 'queue') {
+    return musicPlayer.showQueue(interaction);
+  }
 }
