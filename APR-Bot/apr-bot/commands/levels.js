4b44e833312905cb6dfc83298a8cd24cd1d54033
Aspker <aspk3r@gmail.com>
2025-05-19 08:13:02 -0400

    Update levels.js

--- APR-Bot/apr-bot/commands/levels.js
+++ APR-Bot/apr-bot/commands/levels.js
@@ -6,6 +6,6 @@ export const data = new SlashCommandBuilder()
   .setDescription('Check your current level and XP');
 
 export async function execute(interaction) {
-  const { level, xp, xpToNext } = getUserXP(interaction.user.id);
+  const { level, xp, xpToNext } = getUserXP(interaction.guild.id, interaction.user.id);
   await interaction.reply(`🌟 You are level ${level} with ${xp}/${xpToNext} XP.`);
 }
