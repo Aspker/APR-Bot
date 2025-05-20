1d2ed7be707fc85c0b4cccec4ab5f5a926d5055f
Aspker <aspk3r@gmail.com>
2025-05-20 17:37:46 -0400

    Update db.js

--- APR-Bot/apr-bot/utils/db.js
+++ APR-Bot/apr-bot/utils/db.js
@@ -1,34 +1,15 @@
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'fs';
+const dbFile = './json_sqlite/db.json';
 
-// Setup __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Path to db.json
-const file = path.join(__dirname, '../json_sqlite/db.json');
-
-// Use JSON file adapter for Lowdb
-const adapter = new JSONFile(file);
-
-// Provide default data as second argument to Low constructor
-const defaultData = { users: [] };
-
-const db = new Low(adapter, defaultData);
-
-async function initDB() {
-  await db.read();
-
-  // If db.json is empty, initialize with default data
-  if (!db.data) {
-    db.data = defaultData;
-  }
-
-  await db.write();
+let data = {};
+if (fs.existsSync(dbFile)) {
+  data = JSON.parse(fs.readFileSync(dbFile));
 }
 
-await initDB();
+process.on('exit', () => fs.writeFileSync(dbFile, JSON.stringify(data, null, 2)));
+process.on('SIGINT', () => {
+  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+  process.exit();
+});
 
-export default db;
+export default data;
