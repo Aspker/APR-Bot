6344f468951a9d76a26231883efd81dedd922a6d
Aspker <aspk3r@gmail.com>
2025-05-20 17:34:40 -0400

    Update interactionCreate.js

--- APR-Bot/apr-bot/events/interactionCreate.js
+++ APR-Bot/apr-bot/events/interactionCreate.js
@@ -1,17 +1,16 @@
-// events/interactionCreate.js
 export default {
   name: 'interactionCreate',
-  async execute(interaction, client) {
-    if (!interaction.isCommand()) return;
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
     if (!command) return;
 
     try {
       await command.execute(interaction);
     } catch (error) {
       console.error(error);
-      await interaction.reply({ content: 'There was an error executing that command!', ephemeral: true });
+      await interaction.reply({ content: 'There was an error executing this command.', ephemeral: true });
     }
-  }
+  },
 };
