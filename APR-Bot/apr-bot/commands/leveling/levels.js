7387642c497dd1f8295edf3b016473a474693d9d
Aspker <aspk3r@gmail.com>
2025-05-20 17:25:49 -0400

    Update and rename APR-Bot/apr-bot/commands/levels.js to APR-Bot/apr-bot/commands/leveling/levels.js

---
+++ APR-Bot/apr-bot/commands/leveling/levels.js
@@ -0,0 +1,20 @@
+// commands/leveling/level.js
+import { SlashCommandBuilder } from 'discord.js';
+import { getUserData, getRequiredXp } from '../../utils/xpManager.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('level')
+    .setDescription('Check your level and XP'),
+
+  async execute(interaction) {
+    const userId = interaction.user.id;
+    const guildId = interaction.guild.id;
+    const userData = getUserData(userId, guildId);
+    const requiredXp = getRequiredXp(userData.level);
+
+    await interaction.reply(
+      `🌟 You are level ${userData.level} with ${userdata.xp}/${requiredXp} XP.``
+    );
+  },
+};
