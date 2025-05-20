7ef31615a87f1950e5d00a0507af58e2b777ebb0
Aspker <aspk3r@gmail.com>
2025-05-20 18:02:16 -0400

    Update db.js

--- APR-Bot/apr-bot/utils/db.js
+++ APR-Bot/apr-bot/utils/db.js
@@ -1,15 +1,46 @@
 import fs from 'fs';
+
 const dbFile = './json_sqlite/db.json';
+const backupFile = './json_sqlite/db.backup.json';
 
 let data = {};
-if (fs.existsSync(dbFile)) {
-  data = JSON.parse(fs.readFileSync(dbFile));
+
+function loadDatabase() {
+  try {
+    if (fs.existsSync(dbFile)) {
+      data = JSON.parse(fs.readFileSync(dbFile));
+    }
+  } catch (error) {
+    console.error('Error loading database:', error);
+    if (fs.existsSync(backupFile)) {
+      console.log('Attempting to restore from backup...');
+      data = JSON.parse(fs.readFileSync(backupFile));
+    }
+  }
 }
 
-process.on('exit', () => fs.writeFileSync(dbFile, JSON.stringify(data, null, 2)));
+function saveDatabase() {
+  try {
+    // Create backup before saving
+    if (fs.existsSync(dbFile)) {
+      fs.copyFileSync(dbFile, backupFile);
+    }
+    fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.error('Error saving database:', error);
+  }
+}
+
+loadDatabase();
+
+process.on('exit', () => saveDatabase());
 process.on('SIGINT', () => {
-  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+  saveDatabase();
   process.exit();
+});
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  saveDatabase();
 });
 
 export default data;
