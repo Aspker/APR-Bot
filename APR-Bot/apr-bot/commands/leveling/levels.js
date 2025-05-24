97d8f280a40ad1bdcb0cba2baa07f687ab786ce9
Aspker <aspk3r@gmail.com>
2025-05-24 11:56:53 -0400

    Update levels.js

--- APR-Bot/apr-bot/commands/leveling/levels.js
+++ APR-Bot/apr-bot/commands/leveling/levels.js
@@ -1,17 +1,14 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { getUserData, getRequiredXp } from '../../utils/xpManager.js';
-
 export default {
   data: new SlashCommandBuilder()
     .setName('level')
     .setDescription('Check your level and XP'),
-
   async execute(interaction) {
     const userId = interaction.user.id;
     const guildId = interaction.guild.id;
     const userData = getUserData(userId, guildId);
     const requiredXp = getRequiredXp(userData.level);
-
     await interaction.reply(
       `🌟 You are level ${userData.level} with ${userData.xp}/${requiredXp} XP.`
     );
