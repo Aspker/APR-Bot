4c602a0c45a4aeccfb1b33cfd039390a279d334d
Aspker <aspk3r@gmail.com>
2025-05-24 11:52:41 -0400

    Update messageCreate.js

--- APR-Bot/apr-bot/events/messageCreate.js
+++ APR-Bot/apr-bot/events/messageCreate.js
@@ -1,18 +1,13 @@
 import { addXp } from '../utils/xpManager.js';
-
 const cooldowns = new Map();
-
 export default {
   name: 'messageCreate',
   async execute(message) {
     if (message.author.bot || !message.guild) return;
-
     const key = `${message.guild.id}-${message.author.id}`;
     const now = Date.now();
-
     if (cooldowns.has(key) && now - cooldowns.get(key) < 30000) return;
     cooldowns.set(key, now);
-
     addXp(message.author.id, message.guild.id, 10);
   },
 };
